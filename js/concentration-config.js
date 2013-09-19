46782d166a97e032170132f3eb77a8f55031ea51
Chris Malley <cmalley@pixelzoom.com>
2013-09-19 13:11:44 -0600

    set up the skeleton for the sim

---
+++ js/concentration-config.js
@@ -0,0 +1,46 @@
+// Copyright 2002-2013, University of Colorado Boulder
+
+/*
+ * RequireJS configuration file for the 'Concentration' sim.
+ * Paths are relative to the location of this file.
+ *
+ * @author Chris Malley
+ */
+
+require.config( {
+
+  deps: ['concentration-main'],
+
+  config: {
+    i18n: {
+      locale: 'en_us'
+    }
+  },
+
+  paths: {
+
+    // third-party libs
+    i18n: '../../sherpa/i18n-2.0.4',
+
+    // PhET libs, uppercase names to identify them in require.js imports
+    ASSERT: '../../assert/js',
+    AXON: '../../axon/js',
+    DOT: '../../dot/js',
+    JOIST: '../../joist/js',
+    KITE: '../../kite/js',
+    NITROGLYCERIN: '../../nitroglycerin/js',
+    PHET_CORE: '../../phet-core/js',
+    PHETCOMMON: '../../phetcommon/js',
+    SCENERY: '../../scenery/js',
+    SCENERY_PHET: '../../scenery-phet/js',
+    SUN: '../../sun/js',
+
+    // Dependencies on other sims
+    BEERS_LAW_LAB: "../../beers-law-lab/js",
+
+    //Sim code
+    CONCENTRATION: "."
+  },
+
+  urlArgs: new Date().getTime()  // cache buster to make browser refresh load all included scripts
+} );
---
+++ js/concentration-main.js
@@ -0,0 +1,33 @@
+// Copyright 2002-2013, University of Colorado Boulder
+
+/**
+ * Main entry point for the 'Concentration' sim.
+ *
+ * @author Chris Malley (PixelZoom, Inc.)
+ */
+require( [ 'JOIST/SimLauncher', 'JOIST/Sim', 'BEERS_LAW_LAB/concentration/ConcentrationScreen', 'BEERS_LAW_LAB/common/BLLStrings', 'BEERS_LAW_LAB/common/BLLImages' ],
+  function( SimLauncher, Sim, ConcentrationScreen, BLLStrings, BLLImages ) {
+    'use strict';
+
+    //TODO i18n?
+    var simOptions = {
+      credits: 'PhET Development Team -\n' +
+               'Lead Design: Julia Chamberlain\n' +
+               'Software Development: Chris Malley\n' +
+               'Design Team: Kelly Lancaster, Emily B. Moore, Ariel Paul, Kathy Perkins',
+      thanks: 'Thanks -\n' +
+              'Conversion of this simulation to HTML5 was funded by the Royal Society of Chemistry.'
+    };
+
+    // Appending '?dev' to the URL will enable developer-only features.
+    if ( window.phetcommon.getQueryParameter( 'dev' ) ) {
+      simOptions = _.extend( {
+        // add dev-specific options here
+      }, simOptions );
+    }
+
+    SimLauncher.launch( BLLImages, function() {
+      var sim = new Sim( BLLStrings.concentration, [ new ConcentrationScreen() ], simOptions );
+      sim.start();
+    } );
+  } );
