a0d4592f60d02c5ac71a436cb7c2040b1204a3ae
Chris Malley <cmalley@pixelzoom.com>
2013-10-29 11:34:19 -0600

    joist#69, convert to different requirejs style for main.js

--- js/concentration-main.js
+++ js/concentration-main.js
@@ -5,29 +5,36 @@
  *
  * @author Chris Malley (PixelZoom, Inc.)
  */
-require( [ 'JOIST/SimLauncher', 'JOIST/Sim', 'BEERS_LAW_LAB/concentration/ConcentrationScreen', 'string!BEERS_LAW_LAB/tab.concentration' ],
-  function( SimLauncher, Sim, ConcentrationScreen, simTitle ) {
-    'use strict';
+define( function( require ) {
+  'use strict';
 
-    //TODO i18n?
-    var simOptions = {
-      credits: 'PhET Development Team -\n' +
-               'Lead Design: Julia Chamberlain\n' +
-               'Software Development: Chris Malley\n' +
-               'Design Team: Kelly Lancaster, Emily B. Moore, Ariel Paul, Kathy Perkins',
-      thanks: 'Thanks -\n' +
-              'Conversion of this simulation to HTML5 was funded by the Royal Society of Chemistry.'
-    };
+  // imports
+  var ConcentrationScreen = require( 'BEERS_LAW_LAB/concentration/ConcentrationScreen' );
+  var Sim = require( 'JOIST/Sim' );
+  var SimLauncher = require( 'JOIST/SimLauncher' );
 
-    // Appending '?dev' to the URL will enable developer-only features.
-    if ( window.phetcommon.getQueryParameter( 'dev' ) ) {
-      simOptions = _.extend( {
-        // add dev-specific options here
-      }, simOptions );
-    }
+  // strings
+  var simTitle = require( 'string!BEERS_LAW_LAB/tab.concentration' );
 
-    SimLauncher.launch( function() {
-      var sim = new Sim( simTitle, [ new ConcentrationScreen() ], simOptions );
-      sim.start();
-    } );
+  //TODO i18n?
+  var simOptions = {
+    credits: 'PhET Development Team -\n' +
+             'Lead Design: Julia Chamberlain\n' +
+             'Software Development: Chris Malley\n' +
+             'Design Team: Kelly Lancaster, Emily B. Moore, Ariel Paul, Kathy Perkins',
+    thanks: 'Thanks -\n' +
+            'Conversion of this simulation to HTML5 was funded by the Royal Society of Chemistry.'
+  };
+
+  // Appending '?dev' to the URL will enable developer-only features.
+  if ( window.phetcommon.getQueryParameter( 'dev' ) ) {
+    simOptions = _.extend( {
+      // add dev-specific options here
+    }, simOptions );
+  }
+
+  SimLauncher.launch( function() {
+    var sim = new Sim( simTitle, [ new ConcentrationScreen() ], simOptions );
+    sim.start();
   } );
+} );
