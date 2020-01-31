3634a154717a9beb6a5bdf29af5a32eb53d1af4e
samreid <reids@colorado.edu>
2020-01-31 08:51:14 -0700

    Update slider enabledRange phetioDocumentation, see https://github.com/phetsims/gravity-and-orbits/issues/293

--- js/phet-io/concentration-phet-io-elements-baseline.js
+++ js/phet-io/concentration-phet-io-elements-baseline.js
@@ -1432,7 +1432,7 @@ window.phet.phetio.phetioElementsBaseline = assert &&
       "phetioTypeName": "SliderIO"
     },
     "concentration.concentrationScreen.view.evaporationControl.slider.enabledRangeProperty": {
-      "phetioDocumentation": "",
+      "phetioDocumentation": "Sliders support two ranges: the outer range which specifies the min and max of the track and the enabledRangeProperty, which determines how low and high the thumb can be dragged within the track.",
       "phetioDynamicElement": false,
       "phetioEventType": "MODEL",
       "phetioFeatured": false,
