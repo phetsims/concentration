e4751f4b999513a9b21de73f6561afafa09b12ed
Sam Reid <reids@colorado.edu>
2021-11-18 17:44:01 -0700

    Run grunt update, see https://github.com/phetsims/chipper/issues/1148

--- js/concentrationStrings.ts
+++ js/concentrationStrings.ts
@@ -3,11 +3,17 @@
 /**
  * Auto-generated from modulify, DO NOT manually modify.
  */
-
+/* eslint-disable */
 import getStringModule from '../../chipper/js/getStringModule.js';
 import concentration from './concentration.js';
 
-const concentrationStrings = getStringModule( 'CONCENTRATION' );
+type StringsType = {
+  'concentration': {
+    'title': string
+  }
+};
+
+const concentrationStrings = getStringModule( 'CONCENTRATION' ) as StringsType;
 
 concentration.register( 'concentrationStrings', concentrationStrings );
 
